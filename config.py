9ed59f80ef9751dcd4ef866463a70615a403fcbd
alexander784 <you@example.com>
2024-05-20 11:53:08 +0300

    Generate database

--- config.py
+++ config.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from model import db
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 
 
 app = Flask(__name__)
-app.config['SQLACHEMY_DATABASE)URI'] = 'sqlite:///app.db'
-
-
+# app.config['SQLACHEMY_DATABASE URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app,db)
 
