38508594a4237c3c7a87909276649b898553a9c5
alexander784 <you@example.com>
2024-05-20 21:25:30 +0300

    Create a home Resource URL

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from config import app
-from flask_restful import Api
+from flask_restful import Api,Resource
+from flask import Flask, jsonify,make_response
 
 api = Api(app)
 
+
+class Index(Resource):
+    def get(self):
+        return "Welcome to Towing "
+    
+api.add_resource(Index, "/")
 
 
 if __name__ == "__main__":
