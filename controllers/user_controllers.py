85f0a680bf3895909f522c82cc7a229c07546527
alexander784 <you@example.com>
2024-05-24 20:12:21 +0300

    Get user by Id

--- controllers/user_controllers.py
+++ controllers/user_controllers.py
@@ -6,9 +6,22 @@ from model import User
 
 
 user_bp = Blueprint("user_bp", __name__)
-
+api = Api(user_bp)
 
 class Users(Resource):
     def get(self):
         users = User.query.all()
         return make_response(jsonify(users))
+    
+
+class UserById(Resource):
+    def get(self,user_id):
+        user = User.query.filter_by(id=user_id).first()
+
+        if not user:
+            return make_response(jsonify({"error":"User not found"}), 400)
+        
+        return make_response(jsonify())
+    
+        
+    
