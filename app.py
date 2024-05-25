341a111e43479bea52b1edd4002b153275c37d7a
alexander784 <you@example.com>
2024-05-25 13:32:37 +0300

    Register new user

--- app.py
+++ app.py
@@ -2,10 +2,17 @@ from config import app
 from flask_restful import Api,Resource
 from flask import Flask, jsonify,make_response
 from flasgger import Swagger
+from controllers import auth_controller
+from controllers import user_controller
+
 
 
 api = Api(app)
 swagger = Swagger(app)
+
+api.add_resource(auth_controller)
+api.add_resource(user_controller)
+
 
 
 
--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,7 +1,11 @@
-from flask import jsonify
-from flask_restful import Resource
+from flask import jsonify,make_response
+from flask_restful import Resource,request
 # from flask_mail import Message
 from config import mail
+from model import User,db
+from werkzeug.security import generate_password_hash
+from marshmallow_schema import user_schema
+
 
 
 
@@ -19,3 +23,25 @@ class Index(Resource):
         mail.send(msg)
 
         return "Hello World", 200
+    
+class Register(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        db_user = User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+           return jsonify({"message":f"USer with username already exists"})
+        
+        new_user=User(
+            username = data.get("username"),
+            email=data.get("email"),
+            password=generate_password_hash(data.get("password"))
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return make_response(jsonify(user_schema.dump(new_user)), 201)
+    
+            
