241905ec29799e5d730d73fca42bf6ab1105ecdb
alexander784 <you@example.com>
2024-05-25 13:55:30 +0300

    Authenticate user

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -3,8 +3,10 @@ from flask_restful import Resource,request
 # from flask_mail import Message
 from config import mail
 from model import User,db
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash,check_password_hash
 from marshmallow_schema import user_schema
+from flask_jwt_extended import create_access_token,create_refresh_token
+
 
 
 
@@ -28,10 +30,10 @@ class Register(Resource):
     def post(self):
         data = request.get_json()
         username = data.get("username")
-        db_user = User.query.filter_by(username=username).first()
+        user = User.query.filter_by(username=username).first()
 
-        if db_user is not None:
-           return jsonify({"message":f"USer with username already exists"})
+        if user is not None:
+           return jsonify({"message":f"User with username already exists"})
         
         new_user=User(
             username = data.get("username"),
@@ -44,4 +46,29 @@ class Register(Resource):
 
         return make_response(jsonify(user_schema.dump(new_user)), 201)
     
+
+class login(Resource):
+    def post(self):
+        data = request.get_json()
+        username = data.get("username")
+        password = data.get("password")
+
+        user =User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+
+            access_token = create_access_token(identity=user.username)
+            refresh_token= create_fresh_token(identity=user.username)
+
+            return make_response(jsonify(
+                {"message": "Login successful", "tokens": {
+                  "access_token": access_token,
+                  "refresh_token" : refresh_token },
+                  "user":user_schema.dump(user)}), 200)
+        
+
+
+
+
+
+
             
