02ed87f47ebaebc3ec66fb4bcccd84cd305924bb
alexander784 <you@example.com>
2024-05-27 21:50:06 +0300

    Configuring the secret key

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ from flask_marshmallow import Marshmallow
 from dotenv import load_dotenv
 from flask_jwt_extended import JWTManager
 import os
+from os import environ
 
 load_dotenv()
 
@@ -16,10 +17,11 @@ app = Flask(__name__)
 jwt = JWTManager(app)
 
 
-
+app.secret_key = environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config["JWT_SECRET_KEY"] = environ.get("JWT_SECRET_KEY")
+app.config["SQLALCHEMY_ECHO"] = environ.get("SQLALCHEMY_ECHO")
 
 db.init_app(app)
 bcrypt.init_app(app)
