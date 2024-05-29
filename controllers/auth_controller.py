b2855b8882566c5886bdc148c4c607281f86b8b1
alexander784 <you@example.com>
2024-05-29 13:32:24 +0300

    Minor auth modifications

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -3,11 +3,8 @@ from model import User
 from flask import request, jsonify, make_response
 from werkzeug.security import generate_password_hash, check_password_hash
 from config import db
-from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
 from marshmallow_schema import user_schema
-
-
-
 
 auth_ns = Namespace('auth', description="A namespace for our Authentication")
 
@@ -32,27 +29,24 @@ login_model = auth_ns.model(
 class Signup(Resource):
     @auth_ns.expect(signup_model)
     def post(self):
-        data = request.json
+        try:
+            if ["password"] == ["confirm password"]:
+                new_user = User(
+                    username = ["username"],
+                    email = ["email"],
+                    _password_hash=generate_password_hash(
+                        ["password"].encode('utf-8'))
+                )
+                db.session.add(new_user)
+                db.session.commit()
 
-        username = data.get('username')
-
-        db_user = User.query.filter_by(username=username).first()
-
-        if db_user:
-            return jsonify({"message": f"User with username {username} already exists"}), 409
-
-        new_user = User(
-            username=data.get('username'),
-            email=data.get('email'),
-            password=generate_password_hash(data.get('password'))
-        )
-
-        db.session.add(new_user)
-        db.session.commit()
-        result = user_schema.dump(new_user)
-
-        return  jsonify({"message": "User created successfully", "user": result}), 201
-
+                return make_response(jsonify(user_schema.dump(new_user)), 201)
+            
+            return make_response(jsonify({"error": "Passwords must match"}))
+        except ValueError as e:
+            return make_response(jsonify({"error": [str(e)]}))
+            
+        
 @auth_ns.route('/login')
 class Login(Resource):
     @auth_ns.expect(login_model)
@@ -66,19 +60,20 @@ class Login(Resource):
         
         if db_user and check_password_hash(db_user.password, password):
             access_token = create_access_token(identity=db_user.username)
+            refresh_token = create_refresh_token(identity=db_user.username)
 
             return make_response(jsonify({"message": "Login successful", "tokens": {
-                "access":access_token },
+                "access": access_token,
+                "refresh": refresh_token },
                 "user": user_schema.dump(db_user)}), 200)
         
         return make_response(jsonify({"error":"Invalid username or password"}), 401)
-               
 
 @auth_ns.route('/refresh')
 class RefreshResource(Resource):
+    @jwt_required(refresh=True)
     def post(self):
         current_user = get_jwt_identity()
         new_access_token = create_access_token(identity=current_user)
-        return jsonify({"access_token": new_access_token,
-                        "user":user_schema.dump(current_user)}), 200
+        return jsonify({"access_token": new_access_token}), 200
 
